05e1512dde605bee4aff88a63400d08d06075af7
Carlos Aucaruri <programador@laborfinanciera.com>
2023-12-29 15:52:35 -0500

    added templates

--- settlement/views.py
+++ settlement/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+rooms = [
+    {'id': 1, 'name': 'Lets learn Python!'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Frontend developers'},
+]
+
 # Create your views here.
 def home(request):
-    return HttpResponse('Home Page')
+    context = {'rooms': rooms}
+    return render(request, 'settlement/home.html', context)
 
-def room(request):
-    return HttpResponse('ROOM')
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'settlement/room.html', context)
