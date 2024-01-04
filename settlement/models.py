4590adb2a4f33bf3baddc96da960db501e5c3376
Carlos Aucaruri <carlos.aucaruri@gmail.com>
2024-01-04 16:57:46 -0500

    changed model to handle functions to classify

--- settlement/models.py
+++ settlement/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.serializers.json import DjangoJSONEncoder
+from datetime import datetime
 
 class Settlement(models.Model):
     start_date = models.DateTimeField()
@@ -28,3 +29,29 @@ class SettlementDetails(models.Model):
     daytime_holiday_overtime = models.FloatField(default=0.0)
     night_holiday_overtime = models.FloatField(default=0.0)
     working_shifts = models.JSONField(encoder=DjangoJSONEncoder, default=None)
+
+    def classify_hours(self, start_date: datetime, end_date: datetime):
+        MORNING = 0
+        AFTERNOON = 1
+        EVENING = 2
+
+        print(f'TURN STARTED AT {start_date} AND FINISHED AT {end_date}')
+        if start_date.hour < 10:
+            time_of_day = MORNING
+        elif 10 <= start_date.hour < 17:
+            time_of_day = AFTERNOON
+        elif start_date.hour >= 17:
+            time_of_day = EVENING
+
+        if time_of_day == MORNING:
+            time_difference = end_date - start_date
+            total_hours = time_difference.total_seconds() / 3600
+            remaining_hours = total_hours-8
+            ordinary_hours = 8 if remaining_hours > 0 else total_hours
+            daytime_overtime = remaining_hours if remaining_hours > 0 else 0
+            print(f'HO: {ordinary_hours} | HE: {daytime_overtime}')
+
+        pass
+
+    def classify_week(self):
+        pass
--- settlement/views.py
+++ settlement/views.py
@@ -38,7 +38,6 @@ def process_signing(request, pk):
     settlement = Settlement.objects.get(id=int(pk))
     if request.method == 'POST':
         raw_signings = RawSignings.objects.filter(normalized_date_signed__range=(settlement.start_date, settlement.end_date)).order_by('worker__id', 'normalized_date_signed').all()
-        is_starting_new_week = True
         is_starting_new_day = True
         ordinary_hours = 0
         daytime_overtime = 0
@@ -51,6 +50,7 @@ def process_signing(request, pk):
         start_date_signed = None
         is_inside = True
         current_worker_id = 0
+        settlement_details = None
         for index, raw_signing in enumerate(raw_signings):
             print(raw_signing.get_original_normalized_date_signed(), raw_signing.signed_type, raw_signing.worker.name)
             if is_starting_new_day and raw_signing.signed_type == "S":
@@ -58,40 +58,43 @@ def process_signing(request, pk):
 
             if raw_signing.worker.id != current_worker_id:
                 current_worker_id = raw_signing.worker.id
+                settlement_details, created = SettlementDetails.objects.get_or_create(
+                    settlement=settlement,
+                    worker=raw_signing.worker
+                )
+                # Set defaults of settlement_details to 0
 
             current_date = raw_signing.get_original_normalized_date_signed()
             is_inside = True if raw_signing.signed_type == "E" else False
-
-            if is_starting_new_week and current_date.weekday() == 0:
-                is_starting_new_week = False
 
             if is_starting_new_day and is_inside:
                 start_date_signed = current_date
                 is_starting_new_day = False
 
             if index+1 < len(raw_signings):
+                next_worker_id = raw_signings[index+1].worker.id
                 next_date = raw_signings[index+1].get_original_normalized_date_signed()
+                next_signed_type = raw_signings[index+1].signed_type
+                if next_worker_id != current_worker_id:
+                    settlement_details.classify_hours(start_date_signed, current_date)
+                    settlement_details.classify_week()
+                    # settlement_details.save()
+                    is_starting_new_day = True
+                    continue
                 if not is_inside:
-                    next_signed_type = raw_signings[index+1].signed_type
                     if next_signed_type == "E":
                         hours = get_hours_difference(current_date, next_date)
                         if hours > 4:
                             # The worker is not inside and their next signing was in another day
                             # We can classify the hours now
-                            end_date_signed = current_date
-                            classify_hours(start_date_signed, end_date_signed)
+                            settlement_details.classify_hours(start_date_signed, current_date)
                             is_starting_new_day = True
-                if not is_starting_new_week and next_date.weekday() == 0:
-                    is_starting_new_week = True
-                    # Save the hours classified for the week
-                    pass
             elif index == len(raw_signings)-1:
-                end_date_signed = current_date
-                classify_hours(start_date_signed, end_date_signed)
-                # Save the hours classified for the week
-                pass
+                settlement_details.classify_hours(start_date_signed, current_date)
+                settlement_details.classify_week()
+                # settlement_details.save()
 
-            if index > 23:
+            if index > 50:
                 break
     # return redirect('view', pk=settlement.id)
     return HttpResponse('Entries Processed')
@@ -99,7 +102,3 @@ def process_signing(request, pk):
 def get_hours_difference(start_date: datetime, end_date: datetime) -> int:
     time_difference = end_date - start_date
     return time_difference.total_seconds() / 3600
-
-def classify_hours(start_date: datetime, end_date: datetime):
-    print(f'TURN STARTED AT {start_date} AND FINISHED AT {end_date}')
-    pass
