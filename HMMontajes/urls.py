294ab808eff97a18126a24529ea4716e48f7e001
Carlos Aucaruri <carlos.aucaruri@gmail.com>
2024-01-05 16:14:48 -0500

    added upload-signings as index url

--- HMMontajes/urls.py
+++ HMMontajes/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('__debug__/', include('debug_toolbar.urls')),
     path('settlement/', include('settlement.urls')),
     path('workers/', include('workers.urls')),
+    path('', lambda request: redirect('/workers/upload-signings'), name='index'),
 ]
