e5e8d94da28688d278068c9cdad4263a1ea687e2
Carlos Aucaruri <carlos.aucaruri@gmail.com>
2024-01-06 09:15:57 -0500

    models added to admin

--- settlement/admin.py
+++ settlement/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Settlement, SettlementDetails
 
 # Register your models here.
+admin.site.register(Settlement)
+admin.site.register(SettlementDetails)
