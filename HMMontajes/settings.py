5e1b3d2a1ed4afa657e559f2f76769df06976f8f
Carlos Aucaruri <carlos.aucaruri@gmail.com>
2024-01-08 22:37:40 -0500

    connection changed to mysql

--- HMMontajes/settings.py
+++ HMMontajes/settings.py
@@ -85,12 +85,12 @@ WSGI_APPLICATION = 'HMMontajes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
         'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': os.environ.get('DB_PORT', '5432'),
+        'PORT': os.environ.get('DB_PORT', '3306'),
     }
 }
 
