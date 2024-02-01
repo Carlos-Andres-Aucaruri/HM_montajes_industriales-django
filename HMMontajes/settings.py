3d477b3f5a5c95d2c121a1a46946b81aa2635b7c
Carlos Aucaruri <carlos.aucaruri@gmail.com>
2024-01-31 21:22:04 -0500

    added cors url

--- HMMontajes/settings.py
+++ HMMontajes/settings.py
@@ -154,7 +154,9 @@ if DEBUG:
     ]
 
 # cors authorization
-CORS_ALLOWED_ORIGINS = []
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
