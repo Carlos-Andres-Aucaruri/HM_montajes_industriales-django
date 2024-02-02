c14b537cfafd04f05351c9be489aa0fb804de787
Carlos Aucaruri <carlos.aucaruri@gmail.com>
2024-02-02 13:45:01 -0500

    added JWT for auth

--- HMMontajes/settings.py
+++ HMMontajes/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
+    'rest_framework_simplejwt',
     'coreapi',
     'settlement.apps.SettlementConfig',
     'workers.apps.WorkersConfig',
@@ -160,4 +161,7 @@ CORS_ALLOWED_ORIGINS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
--- HMMontajes/urls.py
+++ HMMontajes/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
 from rest_framework.documentation import include_docs_urls
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,4 +15,7 @@ urlpatterns = [
     path('', lambda request: redirect('/workers/upload-signings'), name='index'),
     path('api/', include('workers.api.urls')),
     path('docs/', include_docs_urls(title='HM API')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
