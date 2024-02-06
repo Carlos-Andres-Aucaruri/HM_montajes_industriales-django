17ca1d1ad090758bf57a4ea712fcef32564d438e
Carlos Aucaruri <carlos.aucaruri@gmail.com>
2024-02-06 14:56:02 -0500

    API docs url can be accessed without permissions

--- HMMontajes/urls.py
+++ HMMontajes/urls.py
@@ -6,6 +6,7 @@ from django.urls import path, include
 from django.shortcuts import redirect
 from rest_framework.documentation import include_docs_urls
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from rest_framework.permissions import AllowAny
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,7 +15,7 @@ urlpatterns = [
     path('workers/', include('workers.urls')),
     path('holidays/', include('holidays.urls')),
     path('', lambda request: redirect('/workers/upload-signings'), name='index'),
-    path('docs/', include_docs_urls(title='HM API')),
+    path('docs/', include_docs_urls(title='HM API', permission_classes=[AllowAny])),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
