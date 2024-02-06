2a6bb3846717863987cd8df59f7d3f3a16fe4999
Carlos Aucaruri <carlos.aucaruri@gmail.com>
2024-02-06 16:46:11 -0500

    added static urls

--- HMMontajes/urls.py
+++ HMMontajes/urls.py
@@ -4,6 +4,8 @@ URL configuration for HMMontajes project.
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework.documentation import include_docs_urls
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 from rest_framework.permissions import AllowAny
@@ -19,4 +21,4 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
