920dee69d130409571ca1aa7da0a3b059da60ec3
Carlos Aucaruri <carlos.aucaruri@gmail.com>
2024-02-12 23:08:08 -0500

    added api endpoint to import signings

--- workers/views.py
+++ workers/views.py
@@ -7,6 +7,9 @@ from datetime import datetime, timezone, timedelta
 import pandas as pd
 from common.util import normalize_date, get_start_end_week_dates
 from rest_framework import viewsets
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes, parser_classes
+from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from .serializers import WorkerSerializer, WorkersSerializer, RawSigningsSerializer, RawSigningsSerializerFull
@@ -52,66 +55,67 @@ def signings(request):
         signings = paginator.page(paginator.num_pages)
     return render(request, 'workers/signings.html', {'signings': signings})
 
+def process_excel(excel_file):
+    df = pd.read_excel(excel_file)
+    dates = []
+    workers = {}
+    for index, row in df.iterrows():
+        if index > 3:
+            document = row.iloc[10]
+            name = row.iloc[2]
+            worker = workers[document] if workers.get(document) is not None else Worker.objects.filter(document=document).first()
+            if not worker:
+                worker = Worker.objects.create(
+                    document=document,
+                    name=name,
+                )
+                workers[document] = worker
+            if workers.get(document) == None:
+                workers[document] = worker
+
+            date_signed = row.iloc[1].replace(tzinfo=timezone(timedelta(hours=-5)))
+            signed_type="E" if row.iloc[4] == "entrada" else "S"
+            normalized_date_signed = normalize_date(date_signed, signed_type)
+
+            if not dates:
+                start_date, end_date = get_start_end_week_dates(normalized_date_signed)
+                dates.append({"start_date": start_date, "end_date": end_date})
+            else:
+                found_in_week = False
+                for date_range in dates:
+                    if date_range["start_date"] <= normalized_date_signed <= date_range["end_date"]:
+                        found_in_week = True
+                        break
+                if not found_in_week:
+                    start_date, end_date = get_start_end_week_dates(normalized_date_signed)
+                    dates.append({"start_date": start_date, "end_date": end_date})
+
+            raw_signing, created = RawSignings.objects.get_or_create(
+                folder_number=row.iloc[0],
+                date_signed=date_signed,
+                worker=worker,
+                signed_type=signed_type,
+                door=row.iloc[5],
+                contract_number=row.iloc[6],
+            )
+            raw_signing.normalized_date_signed = normalized_date_signed
+            raw_signing.save()
+
+    settlements = []
+    for date_range in dates:
+        settlement = Settlement.objects.filter(start_date=date_range["start_date"], end_date=date_range["end_date"]).first()
+        if not settlement:
+            settlement = Settlement(
+                start_date=date_range["start_date"],
+                end_date=date_range["end_date"],
+            )
+            settlements.append(settlement)
+    Settlement.objects.bulk_create(settlements)
+
 def upload_signings(request):
     if request.method == 'POST' and request.FILES['excel_file']:
         excel_file = request.FILES['excel_file']
-        df = pd.read_excel(excel_file)
-
-        dates = []
-        workers = {}
-        for index, row in df.iterrows():
-            if index > 3:
-                document = row.iloc[10]
-                name = row.iloc[2]
-                worker = workers[document] if workers.get(document) is not None else Worker.objects.filter(document=document).first()
-                if not worker:
-                    worker = Worker.objects.create(
-                        document=document,
-                        name=name,
-                    )
-                    workers[document] = worker
-                if workers.get(document) == None:
-                    workers[document] = worker
-
-                date_signed = row.iloc[1].replace(tzinfo=timezone(timedelta(hours=-5)))
-                signed_type="E" if row.iloc[4] == "entrada" else "S"
-                normalized_date_signed = normalize_date(date_signed, signed_type)
-
-                if not dates:
-                    start_date, end_date = get_start_end_week_dates(normalized_date_signed)
-                    dates.append({"start_date": start_date, "end_date": end_date})
-                else:
-                    found_in_week = False
-                    for date_range in dates:
-                        if date_range["start_date"] <= normalized_date_signed <= date_range["end_date"]:
-                            found_in_week = True
-                            break
-                    if not found_in_week:
-                        start_date, end_date = get_start_end_week_dates(normalized_date_signed)
-                        dates.append({"start_date": start_date, "end_date": end_date})
-
-                raw_signing, created = RawSignings.objects.get_or_create(
-                    folder_number=row.iloc[0],
-                    date_signed=date_signed,
-                    worker=worker,
-                    signed_type=signed_type,
-                    door=row.iloc[5],
-                    contract_number=row.iloc[6],
-                )
-                raw_signing.normalized_date_signed = normalized_date_signed
-                raw_signing.save()
-
-        settlements = []
-        for date_range in dates:
-            settlement = Settlement.objects.filter(start_date=date_range["start_date"], end_date=date_range["end_date"]).first()
-            if not settlement:
-                settlement = Settlement(
-                    start_date=date_range["start_date"],
-                    end_date=date_range["end_date"],
-                )
-                settlements.append(settlement)
-        Settlement.objects.bulk_create(settlements)
-
+        process_excel(excel_file)
         return redirect('/settlement/')
 
     return render(request, 'workers/upload_excel.html')
@@ -135,3 +139,13 @@ class SigningView(viewsets.ModelViewSet):
         if self.action == "list":
             return RawSigningsSerializerFull
         return RawSigningsSerializer
+
+@api_view(['POST'])
+@parser_classes([MultiPartParser, FormParser])
+@permission_classes([AllowAny])
+def import_signings(request, format=None):
+    excel_file = request.FILES['excel_file']
+    process_excel(excel_file)
+    signings = RawSignings.objects.all()
+    serializer = RawSigningsSerializer(signings, many=True)
+    return Response(serializer.data)
