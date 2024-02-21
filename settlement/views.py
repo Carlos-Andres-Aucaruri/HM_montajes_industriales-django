6aa857608c19e354b6b4396caa029d8379c873f1
Carlos Aucaruri <carlos.aucaruri@gmail.com>
2024-02-20 21:50:32 -0500

    agregado ordenamiento en liquidacion

--- settlement/views.py
+++ settlement/views.py
@@ -11,6 +11,7 @@ from rest_framework import viewsets
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
+from rest_framework import filters
 from rest_framework import status
 from .serializers import SettlementsSerializer, SettlementSerializer, SettlementDetailsSerializer
 
@@ -191,6 +192,9 @@ def export_settlement(request, pk):
 class SettlementView(viewsets.ModelViewSet):
     serializer_class = SettlementsSerializer
     queryset = Settlement.objects.all()
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = '__all__'
+    ordering = ['-start_date']
     permission_classes = [AllowAny]
 
     def get_serializer_class(self):
